37cb81948ea74dcddb5ee5717e2a2363affa65ea
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-07-24 16:50:32 +0700

    read data with datatables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <title>Akademik - @yield('title')</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +17,11 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">   -->
+    <link  href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+
 </head>
 <body>
     <div id="app">
@@ -49,8 +53,7 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item">
-                                    <!-- <a class="nav-link" href=""></a> -->
+                            <li class="nav-item ">
                                     <a class="nav-link" href="/matakuliah">Matakuliah</a>
                                 </li>
                             <li class="nav-item dropdown">
@@ -60,8 +63,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -80,5 +83,10 @@
             @yield('content')
         </main>
     </div>
+    <script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    </script>
+    @stack('scripts');
 </body>
 </html>
--- resources/views/matakuliah/index.blade.php
+++ resources/views/matakuliah/index.blade.php
@@ -8,10 +8,37 @@
                 <div class="card-header">@yield('title')</div>
 
                 <div class="card-body">
-                    Matakuliah
+                    <table class="table table-hover table-bordered table-striped datatable" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Kode MK</th>
+                                <th>Nama MK</th>
+                                <th>Jumlah sks</th>
+                            </tr>
+                        </thead>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+@push('scripts')
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.datatable').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: '{{ route('matakuliah/json') }}',
+        columns: [
+            {data: 'id', name: 'id'},
+            {data: 'kode_mk', name: 'kode_mk'},
+            {data: 'nama_mk', name: 'nama_mk'},
+            {data: 'jml_sks', name: 'jml_sks'},
+        ]
+    });
+});
+</script>
+@endpush
