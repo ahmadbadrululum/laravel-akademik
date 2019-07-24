62c1765b97f86b7b8bcbb8237282747dfc2e0780
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2019-07-24 15:14:14 +0700

    make controller matakuliah and return view index

---
+++ app/Http/Controllers/MatakuliahController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatakuliahController extends Controller{
+
+    public function index(){
+        return view('matakuliah.index');
+    }
+
+    public function create(){
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     *      * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/matakuliah/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', 'Matakuliah')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">@yield('title')</div>
+
+                <div class="card-body">
+                    Matakuliah
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
